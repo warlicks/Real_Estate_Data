137dc53a24a878cb96ec5132be6e81196902aec1
warlicks <warlick.sean@gmail.com>
2017-01-29 10:47:02 -0800

    Commited changes before archiving.

--- python/xml_parsers.py
+++ python/xml_parsers.py
@@ -84,58 +84,9 @@ def location_parse(xml_string):
 
 		for neighborhood in data_node:
 			name = neighborhood.find('name').text
+			iD = neighborhood.find('id').text
+			
 			output["name"].append(name)
+			output["code"].append(iD)
 
 	return output # Return output for use outside the function. 
-
-def listing_parser(xml_string):
-
-	# Convert the data returned from api to XML
-	xml_element = et.fromstring(xml_string) # Create to Element from string
-	xml_tree = et.ElementTree(xml_element) # Convert Element to Element Tree
-
-	# To correctly parse xml we need to idenitify the api funciton used. 
-	parameters = xml_tree.findall(".//Parameter")
-
-	# There are multiple Parameter nodes, we need to find the child name is "function"
-	for p in parameters:
-		if p.find("name").text == 'function':
-			function = p.find('value').text
-		else:
-			next
-	print function
-
-	# Set Up Storage For Parsed Data
-	output = {"date":[], "type":[], "properties":[], "medianPrice":[], "avgPrice":[]}
-
-	# Each API function has a slightly different schema, thus different logic to parse.  Following if statements set up the logic.
-
-	## getStateStats parsing
-	if function == 'getStateStats':
-		data_node = xml_tree.findall(".//listingStat")
-
-		for records in data_node:
-			record_date = records.find("weekEndingDate").text # Extract record date
-			
-			categories = records.findall(".//subcategory") # Find all subcategory for the given weekend date
-    
-		for category in categories:
-			output["date"].append(record_date) 
-
-			tp = category.find("type").text
-			output["type"].append(tp)
-
-			prop = category.find("numberOfProperties").text
-			output["properties"].append(prop)
-
-			median = category.find("medianListingPrice").text
-			output["medianPrice"].append(median)
-        
-			avg = category.find("averageListingPrice").text
-			output["avgPrice"].append(avg)
-
-	## getCityStats parsing
-	elif function == 'getCityStats':
-		data_node = xml_tree.findal(".//listingStat")
-
-	return output
